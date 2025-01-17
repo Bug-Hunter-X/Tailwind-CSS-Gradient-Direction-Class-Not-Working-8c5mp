d053e03528faa5a0a4adbeb60788d8856ea3c47a
BugHunter-X <askzenai@gmail.com>
2025-01-17 16:36:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// This is the buggy code, the issue is with the class name `bg-gradient-to-r`
+<div className="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg">
+  {/* Content here */}
+</div>
+```
